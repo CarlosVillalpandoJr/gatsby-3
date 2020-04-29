d102f29d2cfa2f2c1443ff8963766131b41a54d1
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-29 17:11:06 -0500

    completed gatsby plugin/layout tut

--- tutorial-part-three/src/pages/index.js
+++ tutorial-part-three/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <Layout>
         <h1>Yooooooooooo</h1>
         <p>
             Welcome, this is a tut on Gatsby Plugins.
         </p>
-    </div>
+    </Layout>
 )
